b35a6324d3fddda402f3fe9dacb05fda588cd236
Ivan Ramos <eng.ivanrff@gmail.com>
2022-10-14 18:14:52 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -31,23 +31,36 @@ function App() {
 
 
   return (
-    <div>
-      <form action="">
-        <label htmlFor="">Tarefa</label>
-        <br />
-        <input value={novaTarefa} type="text" onChange={(e) => setNovaTarefa(e.target.value)}/>
-        <br />
-        <input type="button" onClick={adicionarTarefa} value="Adicionar Tarefa" />
-        <br />
-        <input type="button" onClick={limparTarefa} value="Limpar Tarefa" />
-      </form>
-      <ul>
-        {tarefas.map((umaTarefa,index)=>(
-          <li key={index}>
-            {umaTarefa}, {index+1}
-          </li>
-        ))}
-      </ul>
+    <div id="container">
+
+      <div className='header'>
+        <h2>Gerenciador de Tarefas</h2>
+      </div>
+      <div className='container'>
+        <div className="row">
+          <div className="col">
+            <form action="">
+              <input className='input' value={novaTarefa} type="text" placeholder='Nova Tarefa' onChange={(e) => setNovaTarefa(e.target.value)}/>
+              <br />
+              <input className='btn' type="button" onClick={adicionarTarefa} value="Adicionar Tarefa" />
+              <br />
+              <input className='btn' type="button" onClick={limparTarefa} value="Limpar Tarefas" />
+            </form>
+          </div>
+          <div className='col'>
+            <img id="notepad" src="https://img.freepik.com/premium-vector/simple-diary-spring-memo-with-index-illustration-set-can-be-used-as-note-pad-is-linestyle_632180-299.jpg" alt="something"></img>
+            <ul id="lista-de-tarefas">
+              {tarefas.map((umaTarefa,index)=>(
+                <li key={index}>
+                  {umaTarefa}, {index+1}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+      </div>
+      <div className="footer"></div>
 
     </div>
   );
