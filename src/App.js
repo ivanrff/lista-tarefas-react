679f8a6d8e61adf8b8e50a729cc84514579dad6f
Ivan Ramos <eng.ivanrff@gmail.com>
2022-10-14 15:07:12 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([]);
+  const [novaTarefa, setNovaTarefa] = useState("");
+
+  const adicionarTarefa = () => {
+    if (!novaTarefa) {
+      alert("Tarefa vazia. Preencha novamente");
+      return;
+    };
+    setTarefas([...tarefas, novaTarefa]);
+    setNovaTarefa("")
+  };
+
+  const limparTarefa = () => {
+    setTarefas([]);
+  };
+
+  // useEffect(() => {
+  //   console.log(tarefas);
+  // },[tarefas]);
+
+  // const exibir = (e) => {
+  //   setTarefas(e.target.value);
+  //   console.log(tarefas);
+  // }
+
+
   return (
-    <h1>OIII</h1>
+    <div>
+      <form action="">
+        <label htmlFor="">Tarefa</label>
+        <br />
+        <input value={novaTarefa} type="text" onChange={(e) => setNovaTarefa(e.target.value)}/>
+        <br />
+        <input type="button" onClick={adicionarTarefa} value="Adicionar Tarefa" />
+        <br />
+        <input type="button" onClick={limparTarefa} value="Limpar Tarefa" />
+      </form>
+      <ul>
+        {tarefas.map((umaTarefa,index)=>(
+          <li key={index}>
+            {umaTarefa}, {index+1}
+          </li>
+        ))}
+      </ul>
+
+    </div>
   );
 }
 
